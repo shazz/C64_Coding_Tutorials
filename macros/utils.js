8ac039d09c51ec88ddb00d50cf7ba09c3bbd01a2
Shazz <shazz.ng@gmail.com>
2020-12-17 18:35:39 -0500

    added charset parser

--- macros/utils.js
+++ macros/utils.js
@@ -6,8 +6,9 @@ module.exports = {
         return val.reverse();
     },
     ascii_to_byte: ({}, text, offset) => {
-        var text_without_space = text.split(' ').join('@');
-        const res = Array(text_without_space.length).fill(0).map((v,i) => text_without_space.charCodeAt(i) - offset);
+        // var text_without_space = text.split(' ').join('@');
+        // const res = Array(text_without_space.length).fill(0).map((v,i) => text_without_space.charCodeAt(i) - offset);
+        const res = Array(text.length).fill(0).map((v,i) => text.charCodeAt(i) - offset);
         return res;
     }
 }
